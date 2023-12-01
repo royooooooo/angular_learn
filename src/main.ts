b2e0b44cac745a66c2d9df41143bfa4e0856a5fb
Fu Yi <roy.fuyi@gmail.com>
2023-12-01 16:25:45 +0800

    restructuring the folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Products } from './pages/product/products.component';
+import { Products } from './app/pages/product/products.component';
 
 bootstrapApplication(Products, appConfig)
   .catch((err) => console.error(err));
